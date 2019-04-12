578f089bf69ccd94960a48dcc043afc9ec5b02c4
Anjani kumar joshi <anjani1655@gmail.com>
2019-04-12 22:01:33 +0530

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Anjani joshi',
-    pathPrefix: "/AJPortfolio",
+    pathPrefix: "https://github.com/AnjaniJoshi/AJPortfolio.git",
 
   },
   plugins: [
